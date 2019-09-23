b734a0a297179e1231d000239afc6b00dc37d427
mzeringue16 <54609333+mzeringue16@users.noreply.github.com>
2019-09-22 22:42:05 -0500

    Create Zeringue_hw4.cpp
    Having difficulty with compiler

---
+++ Zeringue_hw4.cpp
@@ -0,0 +1,14 @@
+//Program Name: Small World
+//Purpose: Extend programming functionality and experience
+//Author:Michael Aaron Zeringue
+//Date Last Modified: 09/22/19
+
+#include <iostream>
+using namespace std;
+int main () 
+{
+
+cout << "Hellow World, Michael Zeringue\n";
+return 0;
+
+}
